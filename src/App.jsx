64235b5f03294dbebcff5045487b7decba4f1999
Sumit2424 <mauryasumit3052@gmail.com>
2025-02-22 19:36:23 +0530

    TeacherDashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import AIMentor from './pages/AIMentor'
-import About from './pages/About'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
+import TeacherDashboard from './pages/TeacherDashboard'
+// import Resources from './pages/Resources'
+// import Attendance from './pages/Attendance'
 
 function App() {
   return (
     <Router>
-      <div className="app">
+      <div>
         <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/ai-mentor" element={<AIMentor />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-          </Routes>
-        </main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/ai-mentor" element={<AIMentor />} />
+          <Route path="/teacher-dashboard" element={<TeacherDashboard />} />
+        </Routes>
       </div>
     </Router>
   )
